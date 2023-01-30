582ed2c10da5121e88b04619cfb52feae28bf2fb
DidierOsindero <84969980+DidierOsindero@users.noreply.github.com>
2023-01-30 13:00:51 +0000

    Write fizzBuzz function

---
+++ src/fizzBuzz.ts
@@ -0,0 +1,28 @@
+/**
+ * Given an integer 'n', fizzBuzz returns a string of numbers from 1 to 'n' separated by a comma, where: numbers divisible by 3 are replaced with "Fizz", numbers divisible by 3 are replaced with "Buzz" and numbers divisible by both 3 and 5 are replaced with "FizzBuzz"
+ * @param n - input integer
+ * @returns a string containing incrementing numbers, "fizz", "buzz" and "fizzBuzz"
+ */
+
+export const fizzBuzz = (n: number): string => {
+
+  const fizzBuzzArr = []
+
+  for (let i = 1; i <= n; i++) {
+    const isDivisibleByThree = i % 3 === 0
+    const isDivisibleByFive = i % 5 === 0
+
+    if (isDivisibleByThree && isDivisibleByFive) {
+      fizzBuzzArr.push("FizzBuzz")
+    } else if (isDivisibleByThree) {
+      fizzBuzzArr.push("Fizz")
+    } else if (isDivisibleByFive) {
+      fizzBuzzArr.push("Buzz")
+    } else {
+      fizzBuzzArr.push(i.toString())
+    }
+  }
+
+  const fizzBuzzString = fizzBuzzArr.join(",")
+  return fizzBuzzString
+}
